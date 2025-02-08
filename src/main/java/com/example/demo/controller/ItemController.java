a13d213e7426dbc10743d1ece678ea874a931e2a
sattyajit <sattyajitzarkar11@gmail.com>
2025-02-08 22:28:48 +0530

    commiting update code

--- src/main/java/com/example/demo/controller/ItemController.java
+++ src/main/java/com/example/demo/controller/ItemController.java
@@ -3,7 +3,13 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.Item;
@@ -20,20 +26,20 @@ public class ItemController {
 	        return itemService.getAllItems();
 	    }
 
-		/*
-		 * @GetMapping("/{id}") public ResponseEntity<Item> getItemById(@PathVariable
-		 * String id) { return itemService.getItemById(id) .map(ResponseEntity::ok)
-		 * .orElse(ResponseEntity.notFound().build()); }
-		 * 
-		 * @PostMapping public Item createItem(@RequestBody Item item) { return
-		 * itemService.createItem(item); }
-		 * 
-		 * @PutMapping("/{id}") public ResponseEntity<Item> updateItem(@PathVariable
-		 * String id, @RequestBody Item item) { return
-		 * ResponseEntity.ok(itemService.updateItem(id, item)); }
-		 * 
-		 * @DeleteMapping("/{id}") public ResponseEntity<Void> deleteItem(@PathVariable
-		 * String id) { itemService.deleteItem(id); return
-		 * ResponseEntity.noContent().build(); }
-		 */
+		
+		  @GetMapping("/{id}") public ResponseEntity<Item> getItemById(@PathVariable
+		  String id) { return itemService.getItemById(id) .map(ResponseEntity::ok)
+		  .orElse(ResponseEntity.notFound().build()); }
+		  
+		  @PostMapping public Item createItem(@RequestBody Item item) { return
+			  itemService.createItem(item); }
+		  
+		  @PutMapping("/{id}") public ResponseEntity<Item> updateItem(@PathVariable
+		  String id, @RequestBody Item item) { return
+		  ResponseEntity.ok(itemService.updateItem(id, item)); }
+		  
+		  @DeleteMapping("/{id}") public ResponseEntity<Void> deleteItem(@PathVariable
+		  String id) { itemService.deleteItem(id); return
+		  ResponseEntity.noContent().build(); }
+		 
 }
